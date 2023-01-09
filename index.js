caac86cc95602203eb9553f05a2ef3d204a05fe1
tiagofredk <tiagofredk@gmail.com>
2023-01-09 12:37:46 +0100

    app.listen

--- index.js
+++ index.js
@@ -37,4 +37,4 @@ app.get("/", (req, res)=> {
     })
 })
 
-server.listen(5000, () => console.log("Server Running on port 5000"));
+app.listen(5000, () => console.log("Server Running on port 5000"));
