c28c002030e57e866b68423b9fafad9e1cfd448e
tiagofredk <tiagofredk@gmail.com>
2023-01-09 12:16:15 +0100

    app.use(express.json());

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 // const cors = require("cors");
 
 // app.use(cors())
-
+app.use(express.json());
 const io = require("socket.io")(server/* , {
     cors: {
         origin: ["http://localhost:3000"],
