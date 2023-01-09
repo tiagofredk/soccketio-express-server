1f0ac0e31342431f291ad089be748ba679ec2c36
tiagofredk <tiagofredk@gmail.com>
2023-01-09 11:55:20 +0100

    ok

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors())
+// app.use(cors())
 
-const io = require("socket.io")(server, {
+const io = require("socket.io")(server/* , {
     cors: {
         origin: ["http://localhost:3000"],
         methods: ["GET", "POST"]
     },
-});
+} */);
 
 io.on("connection", socket => {
 
