aae3eed53413cb274d909d473225333992fbfeda
tiagofredk <tiagofredk@gmail.com>
2023-01-09 11:52:48 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const cors = require("cors");
+
+app.use(cors())
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"]
+    },
+});
+
+io.on("connection", socket => {
+
+    console.log("User Connected", socket.id);
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit("chat message", msg);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1>Socket Server</h1>');
+});
+
+server.listen(5000, () => console.log("Server Running on port 5000"));
