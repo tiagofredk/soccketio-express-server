d2772d0081fa4e9eacc8294a3939e4df945260dc
tiagofredk <tiagofredk@gmail.com>
2023-01-31 09:02:09 +0100

    DAILY

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const authController = require("../controllers/authController")
+const protect = require("../midleware/protect")
 
 router.post("/login", authController.login );
 
 router.post("/register", authController.register );
 
-router.get('/', function(req, res, next) {
-  res.send(`<h1 style={{text-align: "center"}}>Express Socket Server</h1>`);
-});
-// after this point routes are protected
 
+// after this point routes are protected
+router.use(protect)
+
+router.get('/', function(req, res, next) {
+  try {
+    res.send(`<h1 style={{text-align: "center"}}>Express Socket Server</h1>`);
+  } catch (error) {
+    next(error)
+  }
+});
 /* GET home page. */
+app.get('/protegido', authController.verifyToken, (req, res) => {
+  // rota protegida aqui
+});
 
 module.exports = router;
--- schemas/schemas.js
+++ schemas/schemas.js
@@ -82,7 +82,6 @@ UserSchema.pre("save", async function (next) {
     if (!this.isModified("password")) {
         next();
     }
-
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
@@ -93,7 +92,6 @@ UserSchema.methods.matchPassword = async function (password) {
 };
 
 UserSchema.methods.getSignedJwtToken = function () {
-    console.log(this._id);
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE,
     });
